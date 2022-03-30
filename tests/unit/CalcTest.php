25d8291b77699ef6919e30b11ea5ea0702e32681
Alexey Latkovskyi <latkovskyi@gmail.com>
2022-03-30 18:23:29 +0300

    unit

---
+++ tests/unit/CalcTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tests\unit;
+
+use App\Calc;
+use Codeception\Test\Unit;
+
+class CalcTest extends Unit
+{
+    protected \UnitTester $tester;
+
+    public function testAdd()
+    {
+        $calc = new Calc();
+
+        $this->assertEquals(5, $calc->add(2,3));
+    }
+}
